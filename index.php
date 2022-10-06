1c3237040661f3c22dcd726cf4a77d45fa480b2b
EthnSchwartz <108826520+Expert0ne@users.noreply.github.com>
2022-10-06 09:27:49 -0300

    teste-web

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include_once './includes/_header.php';
+    ?>
+
+    <main>
+        <h1>Index</h1>
+    </main>
+    <?php
+   
+    include_once './includes/_aside.php';
+    include_once './includes/_footer.php';
+    ?>
+
+   
